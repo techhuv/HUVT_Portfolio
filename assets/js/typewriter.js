a8e2b02ade0909b04bd1d16ff7660f6a62fc2d08
Urvi3012 <urvijain3012@gmail.com>
2021-04-16 21:56:44 +0530

    About Section and Picture

--- assets/js/typewriter.js
+++ assets/js/typewriter.js
@@ -1,6 +1,6 @@
 let typed = new Typed(".typing", {
-    strings: ["Hi I am Sahil.", "This typing animation is very easy.", "You can easily make animation like this.", "And don't forget to Follow Me.","Thanks!"],
+    strings: ["Freelancers", "Python Developers", "Data and Web Scraping Experts", "Data Analysts","Automation Experts","Machine Learning Enthusiasts"],
     typeSpeed: 100,
-    backSpeed: 60,
+    backSpeed: 70,
     loop: true,
   });
