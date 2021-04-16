224440106978c6c7eafef2080a24efa60dbf8f93
Urvi3012 <urvijain3012@gmail.com>
2021-04-16 14:47:16 +0530

    Typewriter Working

---
+++ assets/js/typewriter.js
@@ -0,0 +1,6 @@
+let typed = new Typed(".typing", {
+    strings: ["Hi I am Sahil.", "This typing animation is very easy.", "You can easily make animation like this.", "And don't forget to Follow Me.","Thanks!"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
